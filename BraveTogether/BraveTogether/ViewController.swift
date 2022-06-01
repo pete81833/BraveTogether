005c0f80b0efedacf6982898e99e31f4a0a80e6b
Pete <pete81833@gmail.com>
2022-06-01 20:45:49 +0800

    first commit

---
+++ BraveTogether/BraveTogether/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BraveTogether
+//
+//  Created by 胡丕 on 2022/6/1.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
